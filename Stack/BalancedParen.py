47ac4fc3cdb4f77ed72e4fdedc6b0f931ea8320e
nithinsraju <50978639+nithinsraju@users.noreply.github.com>
2020-11-20 22:12:11 +0530

    Update BalancedParen.py

--- Stack/BalancedParen.py
+++ Stack/BalancedParen.py
@@ -1,4 +1,22 @@
-from Stack import stack
+class stack():
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def is_empty(self):
+        return self.items == []
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+    def get_stack(self):
+        return self.items
 
 
 def is_match(p1, p2):

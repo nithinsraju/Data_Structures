27bf4dc06b1d9f3755d5068e3c015527af52bfc4
nithinsraju <50978639+nithinsraju@users.noreply.github.com>
2020-11-20 15:58:05 +0530

    First Commit

---
+++ SLL/MergeSortedList.py
@@ -0,0 +1,122 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node.next
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def prepend(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_after_node(self, prev_node, data):
+
+        if not prev_node:
+            print("Previous node is not in the list")
+            return
+
+        new_node = Node(data)
+
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+    def delete_node(self, key):
+        cur_node = self.head
+
+        if cur_node and cur_node.data == key:
+            self.head = cur_node.next
+            cur_node = None
+            return
+
+        prev = None
+
+        while cur_node and cur_node.data != key:
+            prev = cur_node
+            cur_node = cur_node.next
+
+        if cur_node is None:
+            return
+
+        prev.next = cur_node.next
+        cur_node = None
+
+    def merge_sorted(self, llist):
+        p = self.head
+        q = llist.head
+        s = None
+
+        if not p:
+            return q
+        if not q:
+            return p
+
+        if p and q:
+            if p.data <= q.data:
+                s = p
+                p = s.next
+            else:
+                s = q
+                q = s.next
+
+            new_head = s
+
+        while p and q:
+            if p.data <= q.data:
+                s.next = p
+                s = p
+                p = s.next
+            else:
+                s.next = q
+                s = q
+                q = s.next
+
+        if not p:
+            s.next = q
+        if not q:
+            s.next = p
+
+        return new_head
+
+llist_1 = LinkedList()
+llist_2 = LinkedList()
+llist_1.append("1")
+llist_1.append("5")
+llist_1.append("7")
+llist_1.append("9")
+llist_1.append("10")
+
+llist_2.append("2")
+llist_2.append("3")
+llist_2.append("4")
+llist_2.append("6")
+llist_2.append("8")
+
+llist_1.print_list()
+print("\n")
+llist_2.print_list()
+print("\n")
+
+llist_1.merge_sorted(llist_2)
+llist_1.print_list()
---
+++ SLL/SwapNodes.py
@@ -0,0 +1,105 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node.next
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def prepend(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_after_node(self, prev_node, data):
+
+        if not prev_node:
+            print("Previous node is not in the list")
+            return
+
+        new_node = Node(data)
+
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+    def delete_node(self, key):
+        cur_node = self.head
+
+        if cur_node and cur_node.data == key:
+            self.head = cur_node.next
+            cur_node = None
+            return
+
+        prev = None
+
+        while cur_node and cur_node.data != key:
+            prev = cur_node
+            cur_node = cur_node.next
+
+        if cur_node is None:
+            return
+
+        prev.next = cur_node.next
+        cur_node = None
+
+    def swap_nodes(self, key_1, key_2):
+
+        if key_1 == key_2:
+            return
+
+        prev_1 = None
+        cur_1 = self.head
+        while cur_1 and cur_1.data != key_1:
+            prev_1 = cur_1
+            cur_1 = cur_1.next
+
+        prev_2 = None
+        cur_2 = self.head
+        while cur_2 and cur_2.data != key_2:
+            prev_2 = cur_2
+            cur_2 = cur_2.next
+
+        if not cur_1 or not cur_2:
+            return
+
+        if prev_1:
+            prev_1.next = cur_2
+        else:
+            self.head = cur_2
+
+        if prev_2:
+            prev_2.next = cur_1
+        else:
+            self.head = cur_1
+
+        cur_1.next, cur_2.next = cur_2.next, cur_1.next
+
+llist = LinkedList()
+llist.append("A")
+llist.append("B")
+llist.append("C")
+llist.append("D")
+
+llist.swap_nodes("A", "D")
+
+llist.print_list()

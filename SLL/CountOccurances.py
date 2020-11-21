f2fd058b470c043e760bcdd80ffa22c8238bb799
nithinsraju <50978639+nithinsraju@users.noreply.github.com>
2020-11-21 12:26:00 +0530

    First Commit

---
+++ SLL/CountOccurances.py
@@ -0,0 +1,98 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node.next
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def prepend(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_after_node(self, prev_node, data):
+
+        if not prev_node:
+            print("Previous node is not in the list")
+            return
+
+        new_node = Node(data)
+
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+    def delete_node(self, key):
+        cur_node = self.head
+
+        if cur_node and cur_node.data == key:
+            self.head = cur_node.next
+            cur_node = None
+            return
+
+        prev = None
+
+        while cur_node and cur_node.data != key:
+            prev = cur_node
+            cur_node = cur_node.next
+
+        if cur_node is None:
+            return
+
+        prev.next = cur_node.next
+        cur_node = None
+
+    def count_occurances_iterative(self, data):
+        count = 0
+        cur = self.head
+        while cur:
+            if cur.data == data:
+                count += 1
+            cur = cur.next
+        return count
+
+    def count_occurances_recursive(self, node, data):
+
+        if not node:
+            return 0
+        if node.data == data:
+            return 1 + self.count_occurances_recursive(node.next, data)
+        else:
+            return self.count_occurances_recursive(node.next, data)
+
+llist = LinkedList()
+
+llist.append(1)
+llist.append(2)
+llist.append(1)
+llist.append(2)
+llist.append(1)
+llist.append(3)
+llist.append(1)
+
+llist.print_list()
+print("\n")
+
+print(llist.count_occurances_iterative(1))
+print("\n")
+print(llist.count_occurances_recursive(llist.head, 2))
